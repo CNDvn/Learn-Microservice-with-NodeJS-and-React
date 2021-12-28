6d1a3efdbef1ac38c06d36085e8da278aa2ca75e
Code Never Die <banhduchieucode@gmail.com>
2021-12-28 09:16:11 +0700

    Event bus implementation
    Tạo event bus và setup để dùng cách giải quyết bài toán quản lý data theo Async

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 const posts = {};
 
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = {
@@ -19,7 +20,23 @@ app.post("/posts", (req, res) => {
     title,
   };
 
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "PostCreated",
+      data: {
+        id,
+        title,
+      },
+    })
+    .catch((err) => console.log(err));
+
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event: ", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
