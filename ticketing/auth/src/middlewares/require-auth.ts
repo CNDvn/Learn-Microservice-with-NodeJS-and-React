342097937689514c560ffb5fa0ed3e9f90b3ee5a
CNDvn <banhduchieucode@gmail.com>
2024-04-30 15:36:35 +0700

    done section 9

---
+++ ticketing/auth/src/middlewares/require-auth.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+import { NotAuthorizedErr } from "../errors/not-authorized-err";
+export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.currentUser) {
+        throw new NotAuthorizedErr()
+    }
+    next()
+}
