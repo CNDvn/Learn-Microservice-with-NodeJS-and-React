8b658efb3dd166e24d31dfef206000a39aa6e26f
CNDvn <banhduchieucode@gmail.com>
2024-05-21 23:00:22 +0700

    section 15

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 console.clear();
 
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -14,25 +15,7 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("accounting-service");
-  const subcription = stan.subscribe(
-    "ticket:created",
-    "queue-group-name",
-    options
-  );
-
-  subcription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
-    }
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
